c59458a40bbbafd6ed64dcd41fa97f2975e05074
datsmiles <tsavaondoaver@gmail.com>
2026-03-27 23:43:31 +0100

    cleaning build errors

--- src/pages/ProductPage.tsx
+++ src/pages/ProductPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 import { useParams, Link } from 'react-router-dom'
 import { ChevronLeft, Star, ShoppingCart, Truck, RotateCcw, ShieldCheck, Plus, Minus } from 'lucide-react'
 import { useCart } from '../context/CartContext'
